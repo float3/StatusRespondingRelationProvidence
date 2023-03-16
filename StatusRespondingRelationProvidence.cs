4ad21019616f703101a3c8e037702eb6e02b5407
hill <hill@hilll.dev>
2023-03-16 22:26:42 +0100

    init

---
+++ StatusRespondingRelationProvidence.cs
@@ -0,0 +1,19 @@
+﻿using ABI_RC.Core.Savior;
+using HarmonyLib;
+using MelonLoader;
+
+[assembly:
+    MelonInfo(typeof(StatusRespondingRelationProvidence.StatusRespondingRelationProvidence),
+        "StatusRespondingRelationProvidence", "1.0.0", "hill")]
+
+namespace StatusRespondingRelationProvidence
+{
+    [HarmonyPatch(typeof(MetaPort), "Start")]
+    public class StatusRespondingRelationProvidence : MelonMod
+    {
+        private static void Postfix()
+        {
+            MetaPort.Instance.matureContentAllowed = true;
+        }
+    }
+}
